42696ae6bd97410a583ef36a82da9215c2338faa
Ac <Ac@DESKTOP-057BF5B>
2022-09-05 11:57:44 +0530

    Pushing the code first time

---
+++ Project2/src/pr/first.java
@@ -0,0 +1,32 @@
+package pr;
+
+public class first {
+	
+	
+		
+		public int team( int b, int c)
+		{
+			
+		int	a= b+c;
+			
+		System.out.println( "two method");
+			
+		return a;
+			
+			
+		}
+		
+		public static void main(String[] args) {
+			
+			
+			first obj=new first();
+			
+			int result=obj.team(20,50);
+			
+			System.out.println("team result is "  +result);
+		}
+	}
+	
+	
+
+
